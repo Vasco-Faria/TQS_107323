1f866ea3280c2be2b2da5c3667c704f2b40ead55
Vasco_Faria <vascomfaria@ua.pt>
2024-03-18 13:59:21 +0000

    Lab5_3 done

--- lab5/lab5_3/src/test/java/tqs/TestRunner.java
+++ lab5/lab5_3/src/test/java/tqs/TestRunner.java
@@ -3,6 +3,8 @@ package tqs;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -11,6 +13,7 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("tqs")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tqs")
 public class TestRunner {
 
